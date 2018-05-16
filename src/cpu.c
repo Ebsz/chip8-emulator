31d3325a973ac9c5154d4651eebceb053afa06c4
Einar Sonju <einarsonju@gmail.com>
2018-05-16 13:54:48 +0200

    Implemented more instructions

--- src/cpu.c
+++ src/cpu.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h> // srand(), rand()
+#include <time.h>	// time()
 #include <SDL2/SDL.h>
 #include "cpu.h"
 #include "mem.h"
@@ -14,15 +16,35 @@ void op_se(uint16_t op);
 void op_sne(uint16_t op);
 void op_se_reg(uint16_t op);
 void op_ld(uint16_t op);
-void op_7(uint16_t op);
+void op_add(uint16_t op);
 void op_8(uint16_t op);
 void op_9(uint16_t op);
 void op_ld_i(uint16_t op);
 void op_B(uint16_t op);
-void op_C(uint16_t op);
+void op_rnd(uint16_t op);
 void op_drw(uint16_t op);
-void op_E(uint16_t op);
+void op_skp(uint16_t op);
 void op_F(uint16_t op);
+
+
+int keyboard_map[16] = {
+	SDL_SCANCODE_X,		
+	SDL_SCANCODE_1,		
+	SDL_SCANCODE_2,		
+	SDL_SCANCODE_3,		
+	SDL_SCANCODE_Q,
+	SDL_SCANCODE_W,
+	SDL_SCANCODE_E,
+	SDL_SCANCODE_A,
+	SDL_SCANCODE_S,
+	SDL_SCANCODE_D,
+	SDL_SCANCODE_Z,
+	SDL_SCANCODE_C,
+	SDL_SCANCODE_4,
+	SDL_SCANCODE_R,
+	SDL_SCANCODE_F,
+	SDL_SCANCODE_Q,
+};
 
 
 // Instructions are based in their most significant digit
@@ -34,19 +56,21 @@ void (*op_base[0x10]) (uint16_t) =
 			op_sne, 	//0x4xxx
 			op_se_reg, 	//0x5xxx
 			op_ld, 		//0x6xxx
-			op_7, 		//0x7xxx
+			op_add, 	//0x7xxx
 			op_8, 	//0x8xxx
 			op_9, //0x9xxx
 			op_ld_i, //0xAxxx
 			op_B, //0xBxxx
-			op_C, //0xCxxx
+			op_rnd, //0xCxxx
 			op_drw, //0xDxxx
-			op_E, //0xExxx
+			op_skp, //0xExxx
 			op_F, //0xFxxx
 };
 
 void cpu_init()
 {
+	srand(time(NULL));
+
 	PC_REG = 0x200;
 	SP_REG = 0x0;
 	STACK_PTR = 0;
@@ -164,7 +188,7 @@ void op_ld(uint16_t op)
 }
 
 // 7xkk -  Vx += kk
-void op_7(uint16_t op)
+void op_add(uint16_t op)
 {
 	uint8_t kk = op%0x100;
 	uint8_t x = (op/0x100)%0x10;
@@ -192,19 +216,68 @@ void op_8(uint16_t op)
 			GP_REG[x] = GP_REG[x] & GP_REG[y];
 			break;
 		
-		case 3: 	// 8xy1 - Vx = Vx XOR Vy
+		case 3: 	// 8xy3 - Vx = Vx XOR Vy
 			GP_REG[x] = GP_REG[x] ^ GP_REG[y];
 			break;
 
-		case 4:
+		case 4:		// 8xy4 - Vx = Vx + Vy
+			if ((GP_REG[x] + GP_REG[y]) > 255) {
+				GP_REG[0xF] = 1;
+			} else {
+				GP_REG[0xF] = 0;
+			}
+			GP_REG[x] = GP_REG[x] + GP_REG[y];
+			break;
+
+		case 5:		// 8xy5 - Vx = Vx - Vy
+			if(GP_REG[x] > GP_REG[y]) {
+				GP_REG[0xF] = 1;
+			} else {
+				GP_REG[0xF] = 0;
+			}
+			GP_REG[x] = GP_REG[x] - GP_REG[y];
+			break;
+
+		case 6:		// 8xy6 - shift Vx right
+			if (GP_REG[x] & 0x1) { // if LSB is 1
+				GP_REG[0xF] = 1;
+			} else {
+				GP_REG[0xF] = 0;
+			}
+			
+			GP_REG[x] /= 2;
+
+			break;
+
+		case 7:		// 8xy7 - Vx = Vy - Vx
+			if (GP_REG[y] > GP_REG[x]) {
+				GP_REG[0xF] = 1;
+			} else {
+				GP_REG[0xF] = 0;
+			}
+			GP_REG[x] = GP_REG[y] - GP_REG[x];
+			break;
+
+		case 0xE:
+			if (GP_REG[x] & 0x80) { // if MSB is 1
+				GP_REG[0xF] = 1;
+			} else {
+				GP_REG[0xF] = 0;
+			}
+			GP_REG[x] *= 2;
 			break;
 	}
 }
 
-// 9xxx
+// 9xy0 - Skip next instruction if Vx != Vy
 void op_9(uint16_t op)
 {
-	getchar();
+	uint8_t x = (op/0x100) % 0x10;
+	uint8_t y = (op/0x10) % 0x10;
+	
+	if (GP_REG[x] != GP_REG[y]) {
+		PC_REG += 2;
+	}
 }
 
 // Annn - I-reg = nnn
@@ -214,16 +287,22 @@ void op_ld_i(uint16_t op)
 	I_REG = n;
 }
 
-// Bxxx
+// Bnnn - Jump to nnn + V0
 void op_B(uint16_t op)
 {
-	getchar();
+	uint16_t n = op%0x1000;
+	PC_REG = n + GP_REG[0];
+
 }
 
-// Cxxx
-void op_C(uint16_t op)
+// Cxkk - Vx = Random byte AND kk
+void op_rnd(uint16_t op)
 {
-	getchar();
+	uint8_t x = (op/0x100)%0x10;
+	uint8_t kk = op%0x100;
+	uint8_t r = rand()%256;
+	
+	GP_REG[x] = r & kk;
 }
 
 // Dxyn - draw n byte sprite starting at I_REG, at (Vx, Vy)
@@ -244,14 +323,33 @@ void op_drw(uint16_t op)
 	getchar();
 }
 
-// Exxx
-void op_E(uint16_t op)
+// Exkk - Skip instruction if key is pressed/not pressed
+void op_skp(uint16_t op)
 {
-	getchar();
+	uint8_t x = (op/0x100)%0x10;
+	uint8_t kk = op%0x100;
+
+	SDL_PumpEvents(); // Update state array
+	const Uint8* state = SDL_GetKeyboardState(NULL);
+
+	if (kk == 0x9E) { // skip if key at Vx is pressed
+		if (state[keyboard_map[GP_REG[x]]]) {
+			PC_REG += 2;
+		}
+	}
+
+	if (kk == 0xA1) { // skip if key not pressed
+		if(!state[keyboard_map[GP_REG[x]]]) {
+			PC_REG += 2;
+		}
+	}
+
 }
 
-// Fxxx
+// Fxkk
 void op_F(uint16_t op)
 {
-	getchar();
+	uint8_t x = (op/0x100)%0x10;
+	uint8_t kk = op%0x100;
+
 }
